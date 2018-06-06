f8a1d1ad54608820e70072a97821f4937c6bb997
Raghu Betina <raghu@firstdraft.com>
2018-06-06 20:43:44 +0000

    Direct association between stateorcountries and users

--- app/models/stateorcountry.rb
+++ app/models/stateorcountry.rb
@@ -1,6 +1,9 @@
 class Stateorcountry < ApplicationRecord
   # Direct associations
 
+  has_many   :users,
+             :dependent => :destroy
+
   has_many   :cities,
              :dependent => :destroy
 
