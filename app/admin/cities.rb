fd09b0a90796229a112cca7ff8697b172959251d
Raghu Betina <raghu@firstdraft.com>
2018-06-06 20:44:09 +0000

    Whitelist columns in City for ActiveAdmin

--- app/admin/cities.rb
+++ app/admin/cities.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register City do
+
+ permit_params :name, :stateorcountry_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
